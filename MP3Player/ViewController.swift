e40efd6de75d7aaedbb08d325261d651da22ebd4
kimkibeom <jeremy6019@daum.net>
2020-03-04 13:11:07 +0900

    ver1.0

---
+++ MP3Player/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MP3Player
+//
+//  Created by 503 on 2020/03/04.
+//  Copyright © 2020 ssang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+   
+
+}
+
